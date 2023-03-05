9c02097be0bfbc26a481c10823a9536c403730fc
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-03-04 20:09:03 -1000

    cleaned up

--- js/main.js
+++ js/main.js
@@ -20,39 +20,6 @@ function getUserChoice() {
 }
 
 
-
-
-
-
-
-
-
-// function game() {
-//     let playerScore = 0; 
-//     let computerScore = 0;
-//     for (let n=1; n <= 5; n++) {
-       
-//         let outcome = playRound(playerSelection, computerSelection);
-//         console.log(outcome);
-//         if (playerScore === 3 || computerScore === 3) {
-//             break;
-//         }   else if (outcome == "you lose!") { 
-//             computerScore++;
-//             console.log("you lost the round!");
-//         }   else if (outcome == "its a tie!") {
-//             console.log("round is a draw");
-//         }   else if (outcome == "you win!") {
-//             playerScore++;
-//             console.log("you won the round!");
-//         }
-//     }
-    // if (playerScore > computerScore) {
-    //     console.log("You won the game!");
-    //  }  else if (playerScore < computerScore) {
-    //     console.log("You lost the game!");
-    //  }  else {console.log("its a tie!");}
-// }
-
 const scores = document.querySelector('#scores');
 const final = document.querySelector('#final');
 const player = document.createElement('span');
